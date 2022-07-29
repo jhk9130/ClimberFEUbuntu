95e18b31987ba5ebfe1cf147eefecd84ee038dbc
jhk91 <hyunk66@gmail.com>
2022-07-29 03:12:11 -0400

    update-climber update

--- src/app/update-climber/update-climber.component.ts
+++ src/app/update-climber/update-climber.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Climber } from '../climber';
+import { ClimberService } from '../climber.service';
 
 @Component({
   selector: 'app-update-climber',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateClimberComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  climber: Climber = new Climber();
+  constructor(private climberService: ClimberService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.climberService.getClimberById(this.id).subscribe(data => {
+      this.climber = data;
+    }, error => console.log(error));
+  }
+
+  onSubmit() {
+    this.climberService.updateClimber(this.id, this.climber).subscribe(data => {
+      this.goToClimberList();
+    }, error => console.log(error));
+  }
+
+  goToClimberList() {
+    this.router.navigate(['/climbers']);
   }
 
 }
