2697519c8c2d75c3808f396fa88f1801c0a0b5d1
jhk91 <hyunk66@gmail.com>
2022-07-29 03:21:22 -0400

    app-routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClimberDetailsComponent } from './climber-details/climber-details.component';
+import { ClimberListComponent } from './climber-list/climber-list.component';
+import { CreateClimberComponent } from './create-climber/create-climber.component';
+import { UpdateClimberComponent } from './update-climber/update-climber.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'climbers', component: ClimberListComponent},
+  {path: '', redirectTo: 'climbers', pathMatch: 'full'},
+  {path: 'create-climber', component: CreateClimberComponent},
+  {path: 'update-climber/:id', component: UpdateClimberComponent},
+  {path: 'climber-details/:id', component: ClimberDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
